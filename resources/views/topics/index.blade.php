2e6f23b46a723b26764b2788917011198562a904
Don Wei <don.wei@cdev.global>
2019-05-07 17:02:58 -0400

    topics list

--- resources/views/topics/index.blade.php
+++ resources/views/topics/index.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
-@section('title', 'Topic')
+@section('title', isset($category) ? $category->name : 'Topic')
 
 @section('content')
 
   <div class="row mb-5">
     <div class="col-lg-9 col-md-9 topic-list">
+        @if (isset($category))
+            <div class="alert alert-info" role="alert">
+                {{ $category->name }} ：{{ $category->description }}
+            </div>
+        @endif
       <div class="card ">
-
         <div class="card-header bg-transparent">
           <ul class="nav nav-pills">
             <li class="nav-item"><a class="nav-link active" href="#">Last Reply</a></li>
