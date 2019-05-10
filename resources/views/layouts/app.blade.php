8091abdfa57bc708f675968b45457fba5100cc4e
Don Wei <don.wei@cdev.global>
2019-05-10 16:35:26 -0400

    topic show page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'LaraBBS') - Laravel BBS</title>
-
+    <meta name="description" content="@yield('description', 'LaraBBS')" />
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @yield('styles')
