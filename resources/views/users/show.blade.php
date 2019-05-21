5ebd5e9d482f67e5e8a68fbd99f9ecfdaab96ccf
Don Wei <don.wei@cdev.global>
2019-05-21 17:11:44 -0400

    user replies topics

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -26,14 +26,25 @@
             </div>
             <hr>
 
-
-            <div class="card">
+            <div class="card ">
                 <div class="card-body">
                     <ul class="nav nav-tabs">
-                        <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta's topic</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Ta's replies</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link bg-transparent {{ active_class(if_query('tab', null)) }}" href="{{ route('users.show', $user->id) }}">
+                                Ta Topics
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link bg-transparent {{ active_class(if_query('tab', 'replies')) }}" href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">
+                                Ta's Replies
+                            </a>
+                        </li>
                     </ul>
-                    @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    @if (if_query('tab', 'replies'))
+                        @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
+                    @else
+                        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+                    @endif
                 </div>
             </div>
 
